edc38d56b2509062ef874f36240f186d4d204b9a
psoto-go <pabletesglez@gmail.com>
2021-06-12 15:33:08 +0000

    add

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,49 +3,55 @@ import PropTypes from "prop-types";
 
 export const Navbar = props => {
 	return (
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">
-				{props.title0}
-			</a>
-			<button
-				class="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarText"
-				aria-controls="navbarText"
-				aria-expanded="false"
-				aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav mr-auto"></ul>
-				<span class="navbar-text">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">
-								{props.title1}{" "}
-								<span class="sr-only">(current)</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								{props.title2}
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								{props.title3}
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">
-								{props.title4}
-							</a>
-						</li>
-					</ul>
-				</span>
-			</div>
-		</nav>
+		<div class="d-flex justify-content-around bg-dark mb-3">
+			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="p-2 mr-5">
+					<a class="navbar-brand" href="#">
+						{props.title0}
+					</a>
+				</div>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarText"
+					aria-controls="navbarText"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div className="p-2 ml-5">
+					<div class="collapse navbar-collapse" id="navbarText">
+						<ul class="navbar-nav mr-auto"></ul>
+						<span class="navbar-text">
+							<ul class="navbar-nav mr-auto">
+								<li class="nav-item active">
+									<a class="nav-link" href="#">
+										{props.title1}{" "}
+										<span class="sr-only">(current)</span>
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">
+										{props.title2}
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">
+										{props.title3}
+									</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">
+										{props.title4}
+									</a>
+								</li>
+							</ul>
+						</span>
+					</div>
+				</div>
+			</nav>
+		</div>
 	);
 };
 
